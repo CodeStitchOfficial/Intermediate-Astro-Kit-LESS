c5ead441a5f3efb60e0d7718a463c9afeaefba56
Ryan <glanzrm@gmail.com>
2023-10-17 21:03:45 -0500

    configured netlify cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,42 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import NetlifyCMS from "astro-netlify-cms";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    NetlifyCMS({
+      config: {
+        backend: {
+          name: "git-gateway",
+          branch: "master",
+        },
+        logo_url: "https://codestitch.app/frontend/images/logo.png",
+        media_folder: "public/assets/images/blog",
+        public_folder: "/assets/images/blog",
+        collections: [
+          {
+            name: "blog",
+            label: "Blog",
+            folder: "src/content/blog",
+            create: true,
+            fields: [
+              { label: "Title", name: "title", widget: "string" },
+              { label: "Description", name: "description", widget: "string" },
+              { label: "Author", name: "author", widget: "string" },
+              { label: "Date", name: "date", widget: "datetime" },
+              {
+                label: "Tags",
+                name: "tags",
+                widget: "list",
+                default: ["post"],
+              },
+              { label: "Featured Image", name: "image", widget: "image" },
+              { label: "Image Caption", name: "imageAlt", widget: "string" },
+              { label: "Body", name: "body", widget: "markdown" },
+            ],
+          },
+        ],
+      },
+    }),
+  ],
+});
